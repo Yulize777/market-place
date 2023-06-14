53e9599e4e76f8b1a0abc6428004390806fc6854
Yulize777 <ruslan602100@gmail.com>
2023-06-14 14:54:15 +0600

    assets

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Layout from "./components/layout/Layout";
 import NotFound from "./pages/NotFound/NotFound";
+import "./style.scss"
+import Catalog from "./pages/Catalog/Catalog";
 
 const App = () => {
     return (
         <Routes>
             <Route element={<Layout/>}>
                 <Route element={<Home/>} path={''}/>
+                <Route element={<Catalog/>} path={''}/>
             </Route>
             <Route path={'*'} element={<NotFound/>}/>
         </Routes>
---
+++ src/pages/Catalog/Catalog.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './catalog.module.scss'
+const Catalog = () => {
+    return (
+        <section>
+            <div className="container">
+
+            </div>
+        </section>
+    );
+};
+
+export default Catalog;
