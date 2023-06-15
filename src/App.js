2f5407a61fcc176a13589b44ee1075c8f354d531
Cholpon <cholpon.damirbekova@gmail.cmom>
2023-06-15 16:18:54 +0600

    add login pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import {Route, Routes} from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Layout from "./components/layout/Layout";
-import NotFound from "./pages/NotFound/NotFound";
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Layout from './components/layout/Layout';
+import NotFound from './pages/NotFound/NotFound';
+import Register from './pages/Login/Register';
+import Auth from './pages/Login/Auth';
 
 const App = () => {
-    return (
-        <Routes>
-            <Route element={<Layout/>}>
-                <Route element={<Home/>} path={''}/>
-            </Route>
-            <Route path={'*'} element={<NotFound/>}/>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route element={<Home />} path={''} />
+        <Route element={<Register />} path="/register" />
+        <Route element={<Auth />} path="/auth" />
+      </Route>
+      <Route path={'*'} element={<NotFound />} />
+    </Routes>
+  );
 };
 
 export default App;
