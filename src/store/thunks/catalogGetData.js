448f3cc9fd45f9ff9b41b41c17cd4b927ff3620e
Yulize777 <ruslan602100@gmail.com>
2023-06-20 11:22:06 +0600

    added reducers

--- src/store/thunks/catalogGetData.js
+++ src/store/thunks/catalogGetData.js
@@ -6,7 +6,6 @@ export const getData = createAsyncThunk(
     async (_,{rejectWithValue}) => {
         try {
             const response = await axios("https://jsonplaceholder.typicode.com/photos?_limit=10")
-            console.log(response)
             if (response.status !== 200){
                 throw new Error('error')
             }
